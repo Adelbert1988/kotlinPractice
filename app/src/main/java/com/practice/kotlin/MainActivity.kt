16f56c994216a3e7a9eea0cf1fc23025addaf609
hongwei <chenghw@ziroom.com>
2020-11-15 22:44:31 +0800

    first commit

---
+++ app/src/main/java/com/practice/kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.practice.kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ziroom.kotlinpractice.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
